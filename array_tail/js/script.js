c169c5b872befba39fd0f181f3fb3daf7aae462e
MaxPredathor <max.gilli98@gmail.com>
2023-10-13 16:03:57 +0200

    esercizio 2

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,7 +1,40 @@
-const btn = document.querySelector('button');
+const btn = document.getElementById('button-1');
+const checkBtn = document.getElementById('button-2');
 const inputEl = document.getElementById('data');
+const inputEl2 = document.getElementById('data-2');
 const resultEl = document.querySelector('.alert');
+const resultEl2 = document.querySelector('.alert-2');
+
+const myArray = []
+
 
 btn.addEventListener('click', function(){
+    let userInput = inputEl.value;
+    let randomNumber;
+    
+    for(let x = 1; x <= userInput; x++){
+        randomNumber = getRndInteger(1, 100);
+        console.log(randomNumber);
+        myArray.push(randomNumber);
+
+    }
+    console.log(myArray);
+    let msg = myArray.slice(myArray.length - 5, myArray.length);
+    resultEl.classList.remove('d-none');
+    resultEl.innerHTML = (msg);
+    
   
-})
+});
+
+checkBtn.addEventListener('click', function(){
+    let userInput2 = inputEl2.value;
+    let msg = myArray.slice(myArray.length - userInput2, myArray.length);
+    resultEl2.classList.remove('d-none');
+    resultEl2.innerHTML = (msg);
+    
+});
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
