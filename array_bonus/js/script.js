1468617b849462886619566d044abce6ff739e1e
MaxPredathor <max.gilli98@gmail.com>
2023-10-13 16:32:15 +0200

    ticket es 3

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -2,6 +2,48 @@ const btn = document.querySelector('button');
 const inputEl = document.getElementById('data');
 const resultEl = document.querySelector('.alert');
 
+const firstArray = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+    ]
+
+
+const secondArray = [
+    1,
+    2,
+    3,
+    4
+    ]
+
+
 btn.addEventListener('click', function(){
+
+    let randomNumber;
+    let x = secondArray.length;
+    let y = firstArray.length;
+
+    
+    while(x !== y){
+        randomNumber = getRndInteger(1, 10);
+        secondArray.push(randomNumber);
+
+    }
+    console.log(firstArray);
+    console.log(secondArray);
+
   
 })
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
